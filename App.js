7a0ab5d65622b5e59345ccb1212bef4d710b9be8
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2025-07-20 14:10:46 +0200

    feat

--- App.js
+++ App.js
@@ -1,23 +1,3 @@
-// import { Text, View } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import "./global.css"; 
-// import StackNavigation from './Navigation/stackNavigation';
-// import { StatusBar } from 'react-native';
-
-
-// const Stack = createStackNavigator();
-
-// export default function App() {
-//   return (
-//     <>
-//     <StackNavigation />
-//     <StatusBar style="black" backgroundColor="#4ba26a" color="color" />
-//   </>
-//   );
-// }
-
-
 import React from 'react';
 import { StatusBar, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
